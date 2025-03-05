4cd7b09576608117bc570913e78443bad17a70ae
Minh Nguyễn <godbsea5@gmail.com>
2025-03-06 02:28:22 +0700

    Add project files.

---
+++ Source/UAVNetSim/UAVNetSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UAVNetSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UAVNETSIM_API AUAVNetSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
