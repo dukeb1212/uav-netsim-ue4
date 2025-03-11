098b0024ac439c44cf7067230e8920fb345c3bca
Minh Nguyễn <godbsea5@gmail.com>
2025-03-11 22:54:18 +0700

    mod: Data type Add separate data type classes for better organizing.

---
+++ Source/UAVNetSim/DataStruct/Telemetry.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class UAVNETSIM_API Telemetry
+{
+public:
+	Telemetry();
+	~Telemetry();
+};
