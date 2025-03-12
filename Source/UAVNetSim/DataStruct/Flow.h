7307b3f1de28fd258b07e89c2e6e62a5f943616f
Ng Duc Minh <godbsea5@gmail.com>
2025-03-13 00:53:45 +0700

    feat: Data Struct Add data struc for flow data and telemetry data.

--- Source/UAVNetSim/DataStruct/Flow.h
+++ Source/UAVNetSim/DataStruct/Flow.h
@@ -3,13 +3,44 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Flow.generated.h"
 
 /**
  * 
  */
+
+USTRUCT(BlueprintType)
+struct FFlowData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(BlueprintReadOnly)
+    float MeanDelay;
+
+    UPROPERTY(BlueprintReadOnly)
+    float MeanJitter;
+
+    UPROPERTY(BlueprintReadOnly)
+    float PacketLossL3;
+
+    UPROPERTY(BlueprintReadOnly)
+    int32 RxBytes;
+
+    UPROPERTY(BlueprintReadOnly)
+    int32 RxPackets;
+
+    UPROPERTY(BlueprintReadOnly)
+    int32 TxBytes;
+
+    UPROPERTY(BlueprintReadOnly)
+    int32 TxPackets;
+};
+
 class UAVNETSIM_API Flow
 {
 public:
 	Flow();
 	~Flow();
 };
+
+
--- Source/UAVNetSim/DataStruct/Telemetry.h
+++ Source/UAVNetSim/DataStruct/Telemetry.h
@@ -3,10 +3,33 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Telemetry.generated.h"
 
 /**
  * 
  */
+
+USTRUCT(BlueprintType)
+struct FTelemetryData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(BlueprintReadOnly, Category = "Telemetry")
+    FString Name;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Telemetry")
+    FVector Position;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Telemetry")
+    FVector Velocity;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Telemetry")
+    float BatteryLevel;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Telemetry")
+    bool IsConnected;
+};
+
 class UAVNETSIM_API Telemetry
 {
 public:
