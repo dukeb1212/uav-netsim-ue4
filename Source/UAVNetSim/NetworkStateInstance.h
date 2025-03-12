a37e7e05de7ad199ae7f173548baaabff243b19a
Ng Duc Minh <godbsea5@gmail.com>
2025-03-13 00:56:33 +0700

    mod: Data struct Move data struct to separate file for better organizing.

--- Source/UAVNetSim/NetworkStateInstance.h
+++ Source/UAVNetSim/NetworkStateInstance.h
@@ -5,39 +5,39 @@
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "Zmq/ZmqSubscriber.h"
-//#include "DataType/FlowData.h"
+#include "DataStruct/Flow.h"
 #include "NetworkStateInstance.generated.h"
 
 /**
  * 
  */
 
-USTRUCT(BlueprintType)
-struct FFlowData
-{
-    GENERATED_BODY()
-
-    UPROPERTY(BlueprintReadOnly)
-    float MeanDelay;
-
-    UPROPERTY(BlueprintReadOnly)
-    float MeanJitter;
-
-    UPROPERTY(BlueprintReadOnly)
-    float PacketLossL3;
-
-    UPROPERTY(BlueprintReadOnly)
-    int32 RxBytes;
-
-    UPROPERTY(BlueprintReadOnly)
-    int32 RxPackets;
-
-    UPROPERTY(BlueprintReadOnly)
-    int32 TxBytes;
-
-    UPROPERTY(BlueprintReadOnly)
-    int32 TxPackets;
-};
+//USTRUCT(BlueprintType)
+//struct FFlowData
+//{
+//    GENERATED_BODY()
+//
+//    UPROPERTY(BlueprintReadOnly)
+//    float MeanDelay;
+//
+//    UPROPERTY(BlueprintReadOnly)
+//    float MeanJitter;
+//
+//    UPROPERTY(BlueprintReadOnly)
+//    float PacketLossL3;
+//
+//    UPROPERTY(BlueprintReadOnly)
+//    int32 RxBytes;
+//
+//    UPROPERTY(BlueprintReadOnly)
+//    int32 RxPackets;
+//
+//    UPROPERTY(BlueprintReadOnly)
+//    int32 TxBytes;
+//
+//    UPROPERTY(BlueprintReadOnly)
+//    int32 TxPackets;
+//};
 
 UCLASS()
 class UAVNETSIM_API UNetworkStateInstance : public UGameInstance
